46d39b55ef0cea90d9a58e2ad71d10c098fec83e
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-09-23 14:32:09 +0300

    push

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,5 +1,6 @@
 "use client";
-import Image from "next/image";
+
+import NextImage from "next/image";
 
 import { useEffect, useMemo, useState } from "react";
 import { useDropzone } from "react-dropzone";
@@ -9,7 +10,6 @@ import { Button, Spinner, IconButton } from "@material-tailwind/react";
 import { toast } from "react-toastify";
 import { CameraIcon, XCircleIcon } from "@heroicons/react/24/outline";
 
-import AnimalDetection from "@/components/AnimalDetection";
 
 const styles = {
   focused: {
@@ -26,11 +26,48 @@ const styles = {
 };
 
 const UploadPage = () => {
+
   const [uploadedPhoto, setUploadedPhoto] = useState();
-  const [prediction, setPrediction] = useState(null);
   const [selectedModal, setSelectedModal] = useState("");
   const [imagePath, setImagePath] = useState("");
   const [loading, setIsLoading] = useState(false);
+
+  const draw_image_and_boxes = (file, boxes) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.className = "rounded-lg mt-3";
+    img.onload = () => {
+      const canvas = document.querySelector("#canvas-id");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      ctx.strokeStyle = "#3f51b5";
+      ctx.lineWidth = 3;
+      ctx.font = "62px poppins";
+
+      boxes.forEach(([x1, y1, x2, y2, label, probability]) => {
+        ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+        ctx.fillStyle = "#3f51b5";
+        const width = "300px";
+        ctx.fillRect(x1, y1, width + 10, 25);
+        ctx.fillStyle = "#3f51b5";
+        const probabilityText = document.getElementById("probabilityText");
+        probabilityText.textContent = `${(probability * 100).toFixed(
+          2,
+        )}% it's a ${label}`;
+        probabilityText.className =
+          "absolute max-h-fit rounded-lg bg-indigo-500 px-5 py-3";
+        probabilityText.style.left = `${(x1 / img.width) * 100}%`;
+        probabilityText.style.top = `${(y1 / img.height) * 100}px`;
+        // ctx.fillText(
+        //   `${(probability * 100).toFixed(2)}% it's a ${label}`,
+        //   x1,
+        //   y1 + 18,
+        // );
+      });
+    };
+  };
 
   const {
     acceptedFiles,
@@ -61,20 +98,12 @@ const UploadPage = () => {
 
   useEffect(() => {
     if (acceptedFiles.length > 0) {
-      const fileUrl = acceptedFiles[0];
-      setPrediction(null);
-      setUploadedPhoto(fileUrl);
-      setImagePath(URL.createObjectURL(fileUrl));
+      const file = acceptedFiles[0];
+      setUploadedPhoto(file);
+      draw_image_and_boxes(file, [[157, 212, 853, 1280, "vizsla", 0.95]]);
+      setImagePath(URL.createObjectURL(file));
     }
   }, [acceptedFiles, setUploadedPhoto]);
-
-  const handleRemoveFiles = () => {
-    if (uploadedPhoto) {
-      URL.revokeObjectURL(uploadedPhoto);
-    }
-    setUploadedPhoto(null);
-    acceptedFiles.splice(0, acceptedFiles.length);
-  };
 
   const handleCapturePhoto = async () => {
     try {
@@ -102,8 +131,7 @@ const UploadPage = () => {
         const blob = dataURLtoBlob(imageDataURL);
         const file = new File([blob], "captured_photo.png");
 
-        setPrediction(null);
-        setUploadedPhoto(URL.createObjectURL(file));
+        setImagePath(URL.createObjectURL(file));
 
         acceptedFiles.splice(0, acceptedFiles.length);
         acceptedFiles.push(file);
@@ -131,7 +159,7 @@ const UploadPage = () => {
 
   // Function to send an image for prediction
   const sendImageForPrediction = async () => {
-     if (!selectedModal) {
+    if (!selectedModal) {
       // Handle case where no service is selected
       setIsLoading(false);
       toast.error("Please select a service");
@@ -143,7 +171,6 @@ const UploadPage = () => {
       toast.error("Please select an image");
       return;
     }
-   
 
     const modelUrls = {
       cat: "https://dogs-api-o5xd.onrender.com/detect",
@@ -152,7 +179,7 @@ const UploadPage = () => {
       animal: "https://animalss-api.onrender.com/detect",
     };
     const formData = new FormData();
-    formData.append("image_file", uploadedPhoto);
+    formData.append("image_file", uploadedPhoto, "image_file");
 
     setIsLoading(true);
     try {
@@ -164,8 +191,8 @@ const UploadPage = () => {
       if (response.ok) {
         const responseData = await response.json();
         // Handle the response containing the predictions
-        setPrediction(responseData);
-        // handleRemoveFiles();
+        draw_image_and_boxes(uploadedPhoto, responseData);
+        setImagePath('')
       } else {
         // Handle the error response
         toast.error("Failed to send the image for detection");
@@ -173,7 +200,7 @@ const UploadPage = () => {
 
       setIsLoading(false);
     } catch (error) {
-      console.error(error);
+      console.error("error sending the image", error);
       setIsLoading(false);
       toast.error("Something went wrong");
     }
@@ -247,28 +274,14 @@ const UploadPage = () => {
                 </Button>
               </div>
 
-              <div {...getRootProps({ className: "dropzone", style })} className="flex w-full items-center justify-center">
-                <label
-                  
-                  className="group flex h-60 w-full flex-col rounded-lg border-4 border-dashed p-10 text-center"
-                >
+              <div
+                {...getRootProps({ className: "dropzone", style })}
+                className="flex w-full items-center justify-center"
+              >
+                <label className="group flex h-60 w-full flex-col rounded-lg border-4 border-dashed p-10 text-center">
                   <div className="flex h-full w-full flex-col items-center justify-center text-center  ">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-10 w-10 text-blue-400 group-hover:text-blue-600"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                      />
-                    </svg>
                     <div className="mx-auto -mt-10 flex max-h-48 w-2/5 flex-auto">
-                      <Image
+                      <NextImage
                         src="/upload.svg"
                         className="object-center"
                         width={300}
@@ -282,9 +295,7 @@ const UploadPage = () => {
                       </span>{" "}
                       <Translate>files here</Translate> <br />{" "}
                       <Translate>or</Translate>{" "}
-                      <span
-                        className="text-blue-600 cursor-pointer hover:underline"
-                      >
+                      <span className="cursor-pointer text-blue-600 hover:underline">
                         <Translate>select a file</Translate>
                       </span>
                     </p>
@@ -302,20 +313,19 @@ const UploadPage = () => {
             </p>
             {imagePath && (
               <div className="relative text-center">
-                
                 <IconButton
                   variant="text"
                   disabled={loading}
                   color="red"
-                  onClick={() => setImagePath('')}
+                  onClick={() => setImagePath("")}
                   className="mx-auto my-3 rounded-full "
                 >
                   <XCircleIcon className="h-7 w-7" />
                 </IconButton>
                 <h1 className="mb-3">Selected Image</h1>
-                <Image
+                <NextImage
                   src={imagePath}
-                  className="relative mx-auto my-3 rounded-lg object-center"
+                  className="relative mx-auto my-3 max-w-[17rem] rounded-lg object-center"
                   width={300}
                   height={300}
                   alt="upload an image"
@@ -331,13 +341,22 @@ const UploadPage = () => {
                 className="rounded-full"
                 fullWidth
               >
-                {loading? <Spinner color="green" className="mx-auto" scale={1.7} />:<Translate>Upload</Translate>}
+                {loading ? (
+                  <Spinner color="green" className="mx-auto" scale={1.7} />
+                ) : (
+                  <Translate>Upload</Translate>
+                )}
               </Button>
             </div>
           </form>
-          {prediction && (
-            <AnimalDetection animalData={prediction} imagePath={imagePath} />
-          )}
+
+          <div className="relative">
+            <canvas
+              className="relative mt-5 block max-w-full rounded-lg"
+              id="canvas-id"
+            ></canvas>
+            <p id="probabilityText"></p>
+          </div>
         </div>
       </div>
     </>
